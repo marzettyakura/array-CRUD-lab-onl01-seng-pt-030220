81aa858faf405c49f7a519574c3571df9f8bb26c
pleasant-archiver-6104 <marzettyakura@yahoo.com>
2020-03-10 04:22:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,38 @@
-def create_an_empty_array ==[water]
+def create_an_empty_array 
+  []
 
 end
 
-def create_an_array == [water]
-  puts ["Dasani", "Ozarka","Fiji", "Nestle"]
-
+def create_an_array
+  water = ["Dasani", "Ozarka","Fiji", "Nestle"]
 end
 
-def add_element_to_end_of_array("Dasani", "Ozarka","Fiji","Nestle")
-  puts water<< "Tea"
-
+def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  puts water.unshift
-
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  puts water.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  puts water.shift
+  array.shift
 
 end
 
 def retrieve_element_from_index(array, index_number)
-
+ array[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+ array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+ array.last
 end
+
